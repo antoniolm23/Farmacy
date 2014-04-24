2993855c62f52cec4770bb24912aba82dfae48bb
Antonio <antoniolm23@gmail.com>
2014-04-24 21:06:43 +0200

    added a gui

---
+++ src/addGraphic.java
@@ -0,0 +1,114 @@
+import javax.swing.JFrame;
+import java.util.*;
+import java.awt.Color;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+public class addGraphic extends JFrame {
+	private JTextField txtName;
+	private JTextField txtBoxquantity;
+	private JTextField txtMetricpL;
+	private JTextField txtTakenquantity;
+	private JTextField txtDailyfrequency;
+	private JTextField txtOverallfrequency;
+	private JTextField txtPrescriptiontrueOr;
+	
+	Warehouse w;
+	
+	public addGraphic(Warehouse w1) {
+		
+		w = w1;
+		
+		setTitle("AddDrug");
+		setSize(600, 600);
+		getContentPane().setBackground(new Color(34, 139, 34));
+		getContentPane().setLayout(null);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		txtName = new JTextField();
+		txtName.setText("Name");
+		txtName.setBounds(12, 12, 114, 19);
+		getContentPane().add(txtName);
+		txtName.setColumns(10);
+		
+		txtBoxquantity = new JTextField();
+		txtBoxquantity.setText("BoxQuantity");
+		txtBoxquantity.setBounds(159, 12, 114, 19);
+		getContentPane().add(txtBoxquantity);
+		txtBoxquantity.setColumns(10);
+		
+		txtMetricpL = new JTextField();
+		txtMetricpL.setText("Metric (p, l or g)");
+		txtMetricpL.setBounds(297, 12, 114, 19);
+		getContentPane().add(txtMetricpL);
+		txtMetricpL.setColumns(10);
+		
+		txtTakenquantity = new JTextField();
+		txtTakenquantity.setText("TakenQuantity");
+		txtTakenquantity.setBounds(12, 54, 114, 19);
+		getContentPane().add(txtTakenquantity);
+		txtTakenquantity.setColumns(10);
+		
+		txtDailyfrequency = new JTextField();
+		txtDailyfrequency.setText("DailyFrequency");
+		txtDailyfrequency.setBounds(169, 54, 114, 19);
+		getContentPane().add(txtDailyfrequency);
+		txtDailyfrequency.setColumns(10);
+		
+		txtOverallfrequency = new JTextField();
+		txtOverallfrequency.setText("OverallFrequency");
+		txtOverallfrequency.setBounds(297, 181, 114, 19);
+		getContentPane().add(txtOverallfrequency);
+		txtOverallfrequency.setColumns(10);
+		
+		JLabel lblEvery = new JLabel("1 = every day");
+		lblEvery.setBounds(287, 124, 104, 15);
+		getContentPane().add(lblEvery);
+		
+		JLabel lblOccasionally = new JLabel("0 = occasionally");
+		lblOccasionally.setBounds(277, 97, 114, 15);
+		getContentPane().add(lblOccasionally);
+		
+		JLabel lblAlternate = new JLabel("2 = alternate days");
+		lblAlternate.setBounds(288, 151, 148, 15);
+		getContentPane().add(lblAlternate);
+		
+		txtPrescriptiontrueOr = new JTextField();
+		txtPrescriptiontrueOr.setText("Prescription (true or false)");
+		txtPrescriptiontrueOr.setBounds(22, 95, 174, 19);
+		getContentPane().add(txtPrescriptiontrueOr);
+		txtPrescriptiontrueOr.setColumns(10);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.addMouseListener(new MouseAdapter() {
+			@Override
+			//get the various fields
+			public void mouseClicked(MouseEvent e) {
+				String n = txtName.getText();
+				int q = Integer.parseInt(txtBoxquantity.getText());
+				char u = txtName.getText().charAt(0);
+				float f = Float.parseFloat(txtTakenquantity.getText());
+				boolean b = Boolean.parseBoolean(txtPrescriptiontrueOr.getText());
+				int freq = Integer.parseInt(txtOverallfrequency.getText());
+				int df = Integer.parseInt(txtDailyfrequency.getText());
+				Drug d = new Drug(n, q, u, b, 0, freq, f, df);
+				w.addDrug(d);
+				
+				//close the window
+				setVisible(false);
+				dispose();
+			}
+		});
+		btnOk.setBackground(new Color(100, 149, 237));
+		btnOk.setBounds(53, 161, 117, 58);
+		getContentPane().add(btnOk);
+		
+		setVisible(true);
+	}
+}
---
+++ src/mainGraphic.java
@@ -0,0 +1,98 @@
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+public class mainGraphic extends JFrame {
+	
+	Warehouse w = new Warehouse();
+	
+	public mainGraphic() {
+		
+		setTitle("Farmacy Project");
+		getContentPane().setBackground(new Color(0, 128, 0));
+		getContentPane().setLayout(null);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		setSize(500, 500);
+		
+		JButton btnAdddrug = new JButton("AddDrug");
+		btnAdddrug.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				addGraphic ag = new addGraphic(w);
+			}
+		});
+		btnAdddrug.setBackground(new Color(100, 149, 237));
+		btnAdddrug.setBounds(26, 41, 117, 25);
+		getContentPane().add(btnAdddrug);
+		
+		JButton btnTakedrug = new JButton("TakeDrug");
+		btnTakedrug.setBackground(new Color(100, 149, 237));
+		btnTakedrug.setBounds(233, 88, 117, 25);
+		getContentPane().add(btnTakedrug);
+		
+		JButton btnErasedrug = new JButton("EraseDrug");
+		btnErasedrug.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				EraseDrug ed = new EraseDrug(w);
+			}
+		});
+		btnErasedrug.setBackground(new Color(100, 149, 237));
+		btnErasedrug.setBounds(166, 41, 117, 25);
+		getContentPane().add(btnErasedrug);
+		
+		JButton btnReplenishdrug = new JButton("ReplenishDrug");
+		btnReplenishdrug.setBackground(new Color(100, 149, 237));
+		btnReplenishdrug.setBounds(26, 88, 154, 25);
+		getContentPane().add(btnReplenishdrug);
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.setBackground(new Color(100, 149, 237));
+		btnSave.setBounds(26, 231, 117, 25);
+		getContentPane().add(btnSave);
+		
+		JButton btnLoad = new JButton("Load");
+		btnLoad.setBackground(new Color(100, 149, 237));
+		btnLoad.setBounds(233, 231, 117, 25);
+		getContentPane().add(btnLoad);
+		
+		JButton btnInventary = new JButton("Inventary");
+		btnInventary.setBackground(new Color(100, 149, 237));
+		btnInventary.setBounds(311, 41, 117, 25);
+		getContentPane().add(btnInventary);
+		
+		JButton btnDrugtotaketoday = new JButton("DrugToTakeToday");
+		btnDrugtotaketoday.setBackground(new Color(100, 149, 237));
+		btnDrugtotaketoday.setBounds(27, 148, 190, 25);
+		getContentPane().add(btnDrugtotaketoday);
+		
+		JButton btnExpiringdrugs = new JButton("ExpiringDrugs");
+		btnExpiringdrugs.setBackground(new Color(100, 149, 237));
+		btnExpiringdrugs.setBounds(267, 148, 190, 25);
+		getContentPane().add(btnExpiringdrugs);
+		
+		JButton btnPrint = new JButton("Print");
+		btnPrint.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				w.print();
+			}
+		});
+		btnPrint.setBackground(new Color(100, 149, 237));
+		btnPrint.setBounds(166, 183, 117, 25);
+		getContentPane().add(btnPrint);
+		
+		setVisible(true);
+	}
+	
+	public static void main(String args[]) {
+		mainGraphic g = new mainGraphic();
+	}
+	
+}
